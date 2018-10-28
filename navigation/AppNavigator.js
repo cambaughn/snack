1bf543e19469ff2eb46acae29a7d23c1b3f0b93a
Cameron Baughn <cam.baughn@gmail.com>
2018-10-28 15:54:29 -0700

    Broken - try to implement nav redux

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
-import App from './MainTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 
 console.ignoredYellowBox = ['Remote debugger'];
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: App,
+  Main: MainTabNavigator,
 });
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Platform, View, Text } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { reduxifyNavigator, createReactNavigationReduxMiddleware, createNavigationReducer } from 'react-navigation-redux-helpers';
+import { Provider, connect } from 'react-redux';
 
+// Components
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import ExploreScreen from '../screens/ExploreScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import ReadingLesson from '../screens/Lesson/Reading/ReadingLesson';
 import LessonList from '../screens/Lesson/LessonList';
-
 import Drill from '../screens/Lesson/Drill/Drill';
 
-import store from '../redux/store';
+// redux
 
+import { user } from '../redux/reducers';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -67,7 +70,7 @@ const MainStack = createBottomTabNavigator({
 });
 
 
-let StackNav = createStackNavigator(
+const TabNavigator = createStackNavigator(
   {
     Main: {
       screen: MainStack,
@@ -86,11 +89,38 @@ let StackNav = createStackNavigator(
 );
 
 
-export default class App extends Component {
+
+
+
+const navReducer = createNavigationReducer(TabNavigator);
+const appReducer = combineReducers({
+  nav: navReducer,
+  user: user,
+});
+
+// Note: createReactNavigationReduxMiddleware must be run before reduxifyNavigator
+const middleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav,
+);
+
+const App = reduxifyNavigator(AppNavigator, "root");
+const mapStateToProps = (state) => ({
+  state: state.nav,
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(App);
+
+const store = createStore(
+  appReducer,
+  applyMiddleware(middleware),
+);
+
+class Root extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <StackNav />
+        <AppWithNavigationState />
       </Provider>
     );
   }
